57f81b7c23d73d6c1bd062ec15232f0928208fce
Danny Gale <dannygal@amazon.com>
2022-08-28 11:32:45 -0700

    feat(widgets): add histogram widget

--- src/widgets/mod.rs
+++ src/widgets/mod.rs
@@ -24,6 +24,7 @@ pub mod canvas;
 mod chart;
 mod clear;
 mod gauge;
+mod histogram;
 mod list;
 mod paragraph;
 mod reflow;
@@ -42,6 +43,7 @@ pub use self::{
     chart::{Axis, Chart, Dataset, GraphType},
     clear::Clear,
     gauge::{Gauge, LineGauge},
+    histogram::Histogram,
     list::{List, ListItem, ListState},
     paragraph::{Paragraph, Wrap},
     scrollbar::{ScrollDirection, Scrollbar, ScrollbarOrientation, ScrollbarState},
