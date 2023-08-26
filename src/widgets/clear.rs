e9bd736b1a680204fa801a7208cddc477f208680
Josh McKinney <joshka@users.noreply.github.com>
2023-08-26 14:32:23 -0700

    test(clear): test Clear rendering (#432)

--- src/widgets/clear.rs
+++ src/widgets/clear.rs
@@ -35,3 +35,28 @@ impl Widget for Clear {
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::assert_buffer_eq;
+
+    #[test]
+    fn render() {
+        let mut buf = Buffer::with_lines(vec!["xxxxxxxxxxxxxxx"; 7]);
+        let clear = Clear;
+        clear.render(Rect::new(1, 2, 3, 4), &mut buf);
+        assert_buffer_eq!(
+            buf,
+            Buffer::with_lines(vec![
+                "xxxxxxxxxxxxxxx",
+                "xxxxxxxxxxxxxxx",
+                "x   xxxxxxxxxxx",
+                "x   xxxxxxxxxxx",
+                "x   xxxxxxxxxxx",
+                "x   xxxxxxxxxxx",
+                "xxxxxxxxxxxxxxx",
+            ])
+        );
+    }
+}
