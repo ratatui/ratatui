c8ab2d59087f5b475ecf6ffa31b89ce24b6b1d28
Aatu Kajasto <60185743+aatukaj@users.noreply.github.com>
2023-09-05 15:51:05 +0300

    fix(chart): use graph style for top line (#462)
    A bug in the rendering caused the top line of the chart to be rendered
    using the style of the chart, instead of the dataset style. This is
    fixed by only setting the style for the width of the text, and not the
    entire row.
    
    Fixes: https://github.com/ratatui-org/ratatui/issues/379

--- tests/widgets_chart.rs
+++ tests/widgets_chart.rs
@@ -618,3 +618,41 @@ fn widgets_chart_can_have_a_legend() {
 
     terminal.backend().assert_buffer(&expected);
 }
+
+#[test]
+fn widgets_chart_top_line_styling_is_correct() {
+    let backend = TestBackend::new(9, 5);
+    let mut terminal = Terminal::new(backend).unwrap();
+
+    let title_style = Style::default().fg(Color::Red).bg(Color::LightRed);
+    let data_style = Style::default().fg(Color::Blue);
+
+    terminal
+        .draw(|f| {
+            let data: [(f64, f64); 2] = [(0.0, 1.0), (1.0, 1.0)];
+            let widget = Chart::new(vec![Dataset::default()
+                .data(&data)
+                .graph_type(ratatui::widgets::GraphType::Line)
+                .style(data_style)])
+            .y_axis(
+                Axis::default()
+                    .title(Span::styled("abc", title_style))
+                    .bounds([0.0, 1.0])
+                    .labels(create_labels(&["a", "b"])),
+            )
+            .x_axis(Axis::default().bounds([0.0, 1.0]));
+            f.render_widget(widget, f.size());
+        })
+        .unwrap();
+
+    let mut expected = Buffer::with_lines(vec![
+        "b│abc••••",
+        " │       ",
+        " │       ",
+        " │       ",
+        "a│       ",
+    ]);
+    expected.set_style(Rect::new(2, 0, 3, 1), title_style);
+    expected.set_style(Rect::new(5, 0, 4, 1), data_style);
+    terminal.backend().assert_buffer(&expected);
+}
