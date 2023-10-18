b61f65bc20918380f2854253d4301ea804fc7437
Josh McKinney <joshka@users.noreply.github.com>
2023-10-18 04:35:51 -0700

    docs(examples): udpate theme to Aardvark Blue (#574)
    This is a nicer theme that makes the colors pop

--- examples/generate.bash
+++ examples/generate.bash
@@ -25,7 +25,7 @@ for tape in examples/*.tape
 do
     gif=${tape/examples\/}
     gif=${gif/.tape/.gif}
-    vhs $tape --quiet
+    ~/go/bin/vhs $tape --quiet
     # this can be pasted into the examples README.md
     echo "[${gif}]: https://github.com/ratatui-org/ratatui/blob/images/examples/${gif}?raw=true"
 done
@@ -35,4 +35,4 @@ cp target/*.gif examples/
 git add examples/*.gif
 git commit -m 'docs(examples): update images'
 gh pr create
-git sw main
+git sw -
