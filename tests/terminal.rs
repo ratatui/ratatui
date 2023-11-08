151e60c48d9581ca1ce914455e5635f3ec02f828
Danny <dannyburrows@protonmail.com>
2023-11-08 09:14:56 +0000

    refactor: Improving new terminal tests

--- tests/terminal.rs
+++ tests/terminal.rs
@@ -5,7 +5,6 @@ use ratatui::{
     backend::{Backend, TestBackend},
     layout::Rect,
     prelude::Buffer,
-    text::Line,
     widgets::{Paragraph, Widget},
     Terminal, TerminalOptions, Viewport,
 };
@@ -53,28 +52,42 @@ fn terminal_draw_returns_the_completed_frame() -> Result<(), Box<dyn Error>> {
 
 #[test]
 fn terminal_insert_before_moves_viewport() -> Result<(), Box<dyn Error>> {
-    let backend = TestBackend::new(8, 5);
+    // When we have a terminal with 5 lines, and a single line viewport, if we insert a
+    // number of lines less than the `terminal height - viewport height` it should move
+    // viewport down to accommodate the new lines.
+
+    let backend = TestBackend::new(20, 5);
     let mut terminal = Terminal::with_options(
         backend,
         TerminalOptions {
-            viewport: Viewport::Inline(2),
+            viewport: Viewport::Inline(1),
         },
     )?;
 
+    // insert_before cannot guarantee the contents of the viewport remain unharmed
+    // by potential scrolling as such it is necessary to call draw afterwards to
+    // redraw the contents of the viewport over the newly designated area.
     terminal.insert_before(2, |buf| {
-        let lines: Vec<Line> = (0..2).map(|i| Line::from(format!("Line {i}"))).collect();
-        Paragraph::new(lines).render(buf.area, buf);
+        Paragraph::new(vec![
+            "------ Line 1 ------".into(),
+            "------ Line 2 ------".into(),
+        ])
+        .render(buf.area, buf);
     })?;
 
     terminal.draw(|f| {
-        let paragraph = Paragraph::new("Test");
+        let paragraph = Paragraph::new("[---- Viewport ----]");
         f.render_widget(paragraph, f.size());
     })?;
 
     assert_buffer_eq!(
         terminal.backend().buffer().clone(),
         Buffer::with_lines(vec![
-            "Line 0  ", "Line 1  ", "Test    ", "        ", "        ",
+            "------ Line 1 ------",
+            "------ Line 2 ------",
+            "[---- Viewport ----]",
+            "                    ",
+            "                    ",
         ])
     );
 
@@ -83,61 +96,98 @@ fn terminal_insert_before_moves_viewport() -> Result<(), Box<dyn Error>> {
 
 #[test]
 fn terminal_insert_before_scrolls_on_large_input() -> Result<(), Box<dyn Error>> {
-    let backend = TestBackend::new(8, 5);
+    // When we have a terminal with 5 lines, and a single line viewport, if we insert many
+    // lines before the viewport (greater than `terminal height - viewport height`) it should
+    // move the viewport down to the bottom of the terminal and scroll all lines above the viewport
+    // until all have been added to the buffer.
+
+    let backend = TestBackend::new(20, 5);
     let mut terminal = Terminal::with_options(
         backend,
         TerminalOptions {
-            viewport: Viewport::Inline(2),
+            viewport: Viewport::Inline(1),
         },
     )?;
 
-    terminal.insert_before(10, |buf| {
-        let lines: Vec<Line> = (0..10).map(|i| Line::from(format!("Line {i}"))).collect();
-        Paragraph::new(lines).render(buf.area, buf);
+    terminal.insert_before(5, |buf| {
+        Paragraph::new(vec![
+            "------ Line 1 ------".into(),
+            "------ Line 2 ------".into(),
+            "------ Line 3 ------".into(),
+            "------ Line 4 ------".into(),
+            "------ Line 5 ------".into(),
+        ])
+        .render(buf.area, buf);
     })?;
 
     terminal.draw(|f| {
-        let paragraph = Paragraph::new("Test");
+        let paragraph = Paragraph::new("[---- Viewport ----]");
         f.render_widget(paragraph, f.size());
     })?;
 
     assert_buffer_eq!(
         terminal.backend().buffer().clone(),
         Buffer::with_lines(vec![
-            "Line 7  ", "Line 8  ", "Line 9  ", "Test    ", "        ",
+            "------ Line 2 ------",
+            "------ Line 3 ------",
+            "------ Line 4 ------",
+            "------ Line 5 ------",
+            "[---- Viewport ----]",
         ])
     );
-
-    print!("{:?}", terminal.backend().buffer());
 
     Ok(())
 }
 
 #[test]
 fn terminal_insert_before_scrolls_on_many_inserts() -> Result<(), Box<dyn Error>> {
-    let backend = TestBackend::new(8, 5);
+    // This test ensures similar behaviour to `terminal_insert_before_scrolls_on_large_input`
+    // but covers a bug previously present whereby multiple small insertions
+    // (less than `terminal height - viewport height`) would have disparate behaviour to one large
+    // insertion. This was caused by an undocumented cap on the height to be inserted, which has now
+    // been removed.
+
+    let backend = TestBackend::new(20, 5);
     let mut terminal = Terminal::with_options(
         backend,
         TerminalOptions {
-            viewport: Viewport::Inline(2),
+            viewport: Viewport::Inline(1),
         },
     )?;
 
-    for i in 0..4 {
-        terminal.insert_before(1, |buf| {
-            Paragraph::new(Line::raw(format!("Line {i}"))).render(buf.area, buf);
-        })?;
-    }
+    terminal.insert_before(1, |buf| {
+        Paragraph::new(vec!["------ Line 1 ------".into()]).render(buf.area, buf);
+    })?;
+
+    terminal.insert_before(1, |buf| {
+        Paragraph::new(vec!["------ Line 2 ------".into()]).render(buf.area, buf);
+    })?;
+
+    terminal.insert_before(1, |buf| {
+        Paragraph::new(vec!["------ Line 3 ------".into()]).render(buf.area, buf);
+    })?;
+
+    terminal.insert_before(1, |buf| {
+        Paragraph::new(vec!["------ Line 4 ------".into()]).render(buf.area, buf);
+    })?;
+
+    terminal.insert_before(1, |buf| {
+        Paragraph::new(vec!["------ Line 5 ------".into()]).render(buf.area, buf);
+    })?;
 
     terminal.draw(|f| {
-        let paragraph = Paragraph::new("Test");
+        let paragraph = Paragraph::new("[---- Viewport ----]");
         f.render_widget(paragraph, f.size());
     })?;
 
     assert_buffer_eq!(
         terminal.backend().buffer().clone(),
         Buffer::with_lines(vec![
-            "Line 1  ", "Line 2  ", "Line 3  ", "Test    ", "        ",
+            "------ Line 2 ------",
+            "------ Line 3 ------",
+            "------ Line 4 ------",
+            "------ Line 5 ------",
+            "[---- Viewport ----]",
         ])
     );
 
