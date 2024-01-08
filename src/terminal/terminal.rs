c50ff08a630ae59c9aac10f69fe3ce67c2db449c
Dheepak Krishnamurthy <me@kdheepak.com>
2024-01-08 03:51:53 -0500

    feat: Add frame count ✨ (#766)

--- src/terminal/terminal.rs
+++ src/terminal/terminal.rs
@@ -71,6 +71,8 @@ where
     /// Last known position of the cursor. Used to find the new area when the viewport is inlined
     /// and the terminal resized.
     last_known_cursor_pos: (u16, u16),
+    /// Number of frames rendered up until current time.
+    frame_count: usize,
 }
 
 /// Options to pass to [`Terminal::with_options`]
@@ -149,15 +151,18 @@ where
             viewport_area,
             last_known_size: size,
             last_known_cursor_pos: cursor_pos,
+            frame_count: 0,
         })
     }
 
     /// Get a Frame object which provides a consistent view into the terminal state for rendering.
     pub fn get_frame(&mut self) -> Frame {
+        let count = self.frame_count;
         Frame {
             cursor_position: None,
             viewport_area: self.viewport_area,
             buffer: self.current_buffer_mut(),
+            count,
         }
     }
 
@@ -279,10 +284,16 @@ where
         // Flush
         self.backend.flush()?;
 
-        Ok(CompletedFrame {
+        let completed_frame = CompletedFrame {
             buffer: &self.buffers[1 - self.current],
             area: self.last_known_size,
-        })
+            count: self.frame_count,
+        };
+
+        // increment frame count before returning from draw
+        self.frame_count = self.frame_count.wrapping_add(1);
+
+        Ok(completed_frame)
     }
 
     /// Hides the cursor.
