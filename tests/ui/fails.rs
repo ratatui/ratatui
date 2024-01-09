77dea441e5637b1c428c2aa71ea67fb3aac20c12
Dheepak Krishnamurthy <me@kdheepak.com>
2024-01-09 01:11:05 -0500

    feat: Update tests that fail ✨

--- tests/ui/fails.rs
+++ tests/ui/fails.rs
@@ -1,11 +1,16 @@
+use ratatui::prelude::*;
 use ratatui_macros::constraints;
 
-fn test() {
+fn main() {
   constraints![,];
-}
 
-fn test2() {
-  let [a, b] = constraints![ == 1/2, == 2, ];
+  // TODO: Make this compiler error pass
+  let [a, b] = constraints![
+    == 1/2,
+    == 2,
+  ];
   assert_eq!(a, Constraint::Ratio(1, 2));
   assert_eq!(b, Constraint::Length(2));
+
+  let [a, b, c] = constraints![ == 1, == 10%, == 2; 4];
 }
