eaa05cd2b9f58479baad8366165ffb40d6c74be9
Josh McKinney <joshka@users.noreply.github.com>
2024-01-11 03:00:18 -0800

    fix: add tests

--- src/style/color.rs
+++ src/style/color.rs
@@ -283,6 +283,15 @@ mod tests {
     use super::*;
 
     #[test]
+    fn from_u32() {
+        assert_eq!(Color::from_u32(0x000000), Color::Rgb(0, 0, 0));
+        assert_eq!(Color::from_u32(0xFF0000), Color::Rgb(255, 0, 0));
+        assert_eq!(Color::from_u32(0x00FF00), Color::Rgb(0, 255, 0));
+        assert_eq!(Color::from_u32(0x0000FF), Color::Rgb(0, 0, 255));
+        assert_eq!(Color::from_u32(0xFFFFFF), Color::Rgb(255, 255, 255));
+    }
+
+    #[test]
     fn from_rgb_color() {
         let color: Color = Color::from_str("#FF0000").unwrap();
         assert_eq!(color, Color::Rgb(255, 0, 0));
