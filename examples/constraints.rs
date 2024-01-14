ac36f2a3f2b796f18bdfb7f183045dfe53cf50b0
Dheepak Krishnamurthy <me@kdheepak.com>
2024-01-14 18:05:56 -0500

    feat: Make legend sticky and only at top ✨

--- examples/constraints.rs
+++ examples/constraints.rs
@@ -7,7 +7,7 @@ use crossterm::{
 };
 use ratatui::{layout::Constraint::*, prelude::*, widgets::*};
 
-const EXAMPLE_HEIGHT: u16 = 5;
+const EXAMPLE_HEIGHT: u16 = 3;
 
 fn main() -> Result<(), Box<dyn Error>> {
     // setup terminal
@@ -91,6 +91,34 @@ impl App {
             .min(self.max_scroll_offset)
     }
 
+    fn render_tabs_and_legend(&self, area: Rect, buf: &mut Buffer) {
+        let [tabs, legend] = area.split(&Layout::vertical([
+            Constraint::Fixed(3),
+            Constraint::Fixed(3),
+        ]));
+        self.render_tabs(tabs, buf);
+        self.render_legend(legend, buf);
+    }
+
+    fn render_legend(&self, area: Rect, buf: &mut Buffer) {
+        let width = area.width as usize;
+        // a bar like `<----- 80 px ----->`
+        let width_label = format!("{} px", width);
+        let width_bar = format!(
+            "<{width_label:-^width$}>",
+            width = width - width_label.len() / 2
+        );
+        Paragraph::new(width_bar.dark_gray())
+            .alignment(Alignment::Center)
+            .block(Block::default().padding(Padding {
+                left: 0,
+                right: 0,
+                top: 1,
+                bottom: 0,
+            }))
+            .render(area, buf);
+    }
+
     fn render_tabs(&self, area: Rect, buf: &mut Buffer) {
         // ┌Constraints───────────────────────────────────────────────────────────────────┐
         // │  Fixed  │  Length  │  Percentage  │  Ratio  │  Proportional  │  Min  │  Max  │
@@ -122,25 +150,27 @@ impl App {
 
 impl Widget for App {
     fn render(self, area: Rect, buf: &mut Buffer) {
-        let [tabs_area, demo_area] = area.split(&Layout::vertical([Fixed(3), Proportional(0)]));
+        let [tabs_and_legend_area, demo_area] =
+            area.split(&Layout::vertical([Fixed(6), Proportional(0)]));
 
         // render demo content into a separate buffer so all examples fit
         let mut demo_buf = Buffer::empty(Rect::new(
             0,
             0,
             buf.area.width,
-            self.selected_example.get_example_count() * EXAMPLE_HEIGHT,
+            self.selected_example.get_example_count() * EXAMPLE_HEIGHT
+                + tabs_and_legend_area.height,
         ));
 
         self.selected_example.render(demo_buf.area, &mut demo_buf);
 
         // render tabs into a separate buffer
-        let mut tabs_buf = Buffer::empty(tabs_area);
-        self.render_tabs(tabs_area, &mut tabs_buf);
+        let mut tabs_and_legend_buf = Buffer::empty(tabs_and_legend_area);
+        self.render_tabs_and_legend(tabs_and_legend_area, &mut tabs_and_legend_buf);
 
         // Assemble both buffers
         // NOTE: You shouldn't do this in a production app
-        buf.content = tabs_buf.content;
+        buf.content = tabs_and_legend_buf.content;
         buf.content.append(
             &mut demo_buf
                 .content
@@ -237,22 +267,8 @@ impl ExampleSelection {
     fn render_fixed_example(&self, area: Rect, buf: &mut Buffer) {
         let [example1, example2, _] = area.split(&Layout::vertical([Fixed(EXAMPLE_HEIGHT); 3]));
 
-        // Fixed(40), Proportional(0)
-        //
-        // <---------------------50 px---------------------->
-        //
-        // ┌──────────────────────────────────────┐┌────────┐
-        // │                 40 px                ││  10 px │
-        // └──────────────────────────────────────┘└────────┘
         Example::new([Fixed(40), Proportional(0)]).render(example1, buf);
 
-        // Fixed(20), Fixed(20), Proportional(0)
-        //
-        // <---------------------50 px---------------------->
-        //
-        // ┌──────────────────┐┌──────────────────┐┌────────┐
-        // │       20 px      ││       20 px      ││  10 px │
-        // └──────────────────┘└──────────────────┘└────────┘
         Example::new([Fixed(20), Fixed(20), Proportional(0)]).render(example2, buf);
     }
 
@@ -260,40 +276,12 @@ impl ExampleSelection {
         let [example1, example2, example3, example4, _] =
             area.split(&Layout::vertical([Fixed(EXAMPLE_HEIGHT); 5]));
 
-        // Length(20), Fixed(20)
-        //
-        // <---------------------50 px---------------------->
-        //
-        // ┌────────────────────────────┐┌──────────────────┐
-        // │            30 px           ││       20 px      │
-        // └────────────────────────────┘└──────────────────┘
         Example::new([Length(20), Fixed(20)]).render(example1, buf);
 
-        // Length(20), Length(20)
-        //
-        // <---------------------50 px---------------------->
-        //
-        // ┌──────────────────┐┌────────────────────────────┐
-        // │       20 px      ││            30 px           │
-        // └──────────────────┘└────────────────────────────┘
         Example::new([Length(20), Length(20)]).render(example2, buf);
 
-        // Length(20), Min(20)
-        //
-        // <---------------------50 px---------------------->
-        //
-        // ┌──────────────────┐┌────────────────────────────┐
-        // │       20 px      ││            30 px           │
-        // └──────────────────┘└────────────────────────────┘
         Example::new([Length(20), Min(20)]).render(example3, buf);
 
-        // Length(20), Max(20)
-        //
-        // <---------------------50 px---------------------->
-        //
-        // ┌────────────────────────────┐┌──────────────────┐
-        // │            30 px           ││       20 px      │
-        // └────────────────────────────┘└──────────────────┘
         Example::new([Length(20), Max(20)]).render(example4, buf);
     }
 
@@ -301,50 +289,14 @@ impl ExampleSelection {
         let [example1, example2, example3, example4, example5, _] =
             area.split(&Layout::vertical([Fixed(EXAMPLE_HEIGHT); 6]));
 
-        // Percentage(75), Proportional(0)
-        //
-        // <---------------------50 px---------------------->
-        //
-        // ┌────────────────────────────────────────────────┐
-        // │                      50 px                     │
-        // └────────────────────────────────────────────────┘
-        //
         Example::new([Percentage(75), Proportional(0)]).render(example1, buf);
 
-        // Percentage(25), Proportional(0)
-        //
-        // <---------------------50 px---------------------->
-        //
-        // ┌────────────────────────────────────────────────┐
-        // │                      50 px                     │
-        // └────────────────────────────────────────────────┘
         Example::new([Percentage(25), Proportional(0)]).render(example2, buf);
 
-        // Percentage(50), Min(20)
-        //
-        // <---------------------50 px---------------------->
-        //
-        // ┌───────────────────────┐┌───────────────────────┐
-        // │         25 px         ││         25 px         │
-        // └───────────────────────┘└───────────────────────┘
         Example::new([Percentage(50), Min(20)]).render(example3, buf);
 
-        // Percentage(0), Max(0)
-        //
-        // <---------------------50 px---------------------->
-        //
-        // ┌────────────────────────────────────────────────┐
-        // │                      50 px                     │
-        // └────────────────────────────────────────────────┘
         Example::new([Percentage(0), Max(0)]).render(example4, buf);
 
-        // Percentage(0), Proportional(0)
-        //
-        // <---------------------50 px---------------------->
-        //
-        // ┌────────────────────────────────────────────────┐
-        // │                      50 px                     │
-        // └────────────────────────────────────────────────┘
         Example::new([Percentage(0), Proportional(0)]).render(example5, buf);
     }
 
@@ -352,111 +304,35 @@ impl ExampleSelection {
         let [example1, example2, example3, example4, _] =
             area.split(&Layout::vertical([Fixed(EXAMPLE_HEIGHT); 5]));
 
-        // Ratio(1, 2), Ratio(1, 2)
-        //
-        // <---------------------50 px---------------------->
-        //
-        // ┌───────────────────────┐┌───────────────────────┐
-        // │         25 px         ││         25 px         │
-        // └───────────────────────┘└───────────────────────┘
         Example::new([Ratio(1, 2); 2]).render(example1, buf);
 
-        // Ratio(1, 4), Ratio(1, 4), Ratio(1, 4), Ratio(1, 4)
-        //
-        // <---------------------50 px---------------------->
-        //
-        // ┌───────────┐┌──────────┐┌───────────┐┌──────────┐
-        // │   13 px   ││   12 px  ││   13 px   ││   12 px  │
-        // └───────────┘└──────────┘└───────────┘└──────────┘
         Example::new([Ratio(1, 4); 4]).render(example2, buf);
 
-        // Ratio(1, 2), Ratio(1, 3), Ratio(1, 4)
-        //
-        // <---------------------50 px---------------------->
-        //
-        // ┌───────────────────────┐┌───────────────┐┌──────┐
-        // │         25 px         ││     17 px     ││ 8 px │
-        // └───────────────────────┘└───────────────┘└──────┘
         Example::new([Ratio(1, 2), Ratio(1, 3), Ratio(1, 4)]).render(example3, buf);
 
-        // Ratio(1, 2), Percentage(25), Length(10)
-        //
-        // <---------------------50 px---------------------->
-        //
-        // ┌───────────────────────┐┌───────────┐┌──────────┐
-        // │         25 px         ││   13 px   ││   12 px  │
-        // └───────────────────────┘└───────────┘└──────────┘
         Example::new([Ratio(1, 2), Percentage(25), Length(10)]).render(example4, buf);
     }
 
     fn render_proportional_example(&self, area: Rect, buf: &mut Buffer) {
         let [example1, example2, _] = area.split(&Layout::vertical([Fixed(EXAMPLE_HEIGHT); 3]));
 
-        // Proportional(1), Proportional(2), Proportional(3)
-        //
-        // <---------------------50 px---------------------->
-        //
-        // ┌──────┐┌───────────────┐┌───────────────────────┐
-        // │ 8 px ││     17 px     ││         25 px         │
-        // └──────┘└───────────────┘└───────────────────────┘
         Example::new([Proportional(1), Proportional(2), Proportional(3)]).render(example1, buf);
 
-        // Proportional(1), Percentage(50), Proportional(1)
-        //
-        // <---------------------50 px---------------------->
-        //
-        // ┌───────────┐┌───────────────────────┐┌──────────┐
-        // │   13 px   ││         25 px         ││   12 px  │
-        // └───────────┘└───────────────────────┘└──────────┘
         Example::new([Proportional(1), Percentage(50), Proportional(1)]).render(example2, buf);
     }
 
     fn render_min_example(&self, area: Rect, buf: &mut Buffer) {
         let [example1, example2, example3, example4, example5, _] =
             area.split(&Layout::vertical([Fixed(EXAMPLE_HEIGHT); 6]));
-        // Percentage(100), Min(0)
-        //
-        // <---------------------50 px---------------------->
-        //
-        // ┌────────────────────────────────────────────────┐
-        // │                      50 px                     │
-        // └────────────────────────────────────────────────┘
+
         Example::new([Percentage(100), Min(0)]).render(example1, buf);
 
-        // Percentage(100), Min(20)
-        //
-        // <---------------------50 px---------------------->
-        //
-        // ┌────────────────────────────┐┌──────────────────┐
-        // │            30 px           ││       20 px      │
-        // └────────────────────────────┘└──────────────────┘
         Example::new([Percentage(100), Min(20)]).render(example2, buf);
 
-        // Percentage(100), Min(40)
-        //
-        // <---------------------50 px---------------------->
-        //
-        // ┌────────┐┌──────────────────────────────────────┐
-        // │  10 px ││                 40 px                │
-        // └────────┘└──────────────────────────────────────┘
         Example::new([Percentage(100), Min(40)]).render(example3, buf);
 
-        // Percentage(100), Min(60)
-        //
-        // <---------------------50 px---------------------->
-        //
-        // ┌────────────────────────────────────────────────┐
-        // │                      50 px                     │
-        // └────────────────────────────────────────────────┘
         Example::new([Percentage(100), Min(60)]).render(example4, buf);
 
-        // Percentage(100), Min(80)
-        //
-        // <---------------------50 px---------------------->
-        //
-        // ┌────────────────────────────────────────────────┐
-        // │                      50 px                     │
-        // └────────────────────────────────────────────────┘
         Example::new([Percentage(100), Min(80)]).render(example5, buf);
     }
 
@@ -464,50 +340,14 @@ impl ExampleSelection {
         let [example1, example2, example3, example4, example5, _] =
             area.split(&Layout::vertical([Fixed(EXAMPLE_HEIGHT); 6]));
 
-        // Percentage(0), Max(0)
-        //
-        // <---------------------50 px---------------------->
-        //
-        // ┌────────────────────────────────────────────────┐
-        // │                      50 px                     │
-        // └────────────────────────────────────────────────┘
         Example::new([Percentage(0), Max(0)]).render(example1, buf);
 
-        //
-        // Percentage(0), Max(20)
-        //
-        // <---------------------50 px---------------------->
-        //
-        // ┌────────────────────────────┐┌──────────────────┐
-        // │            30 px           ││       20 px      │
-        // └────────────────────────────┘└──────────────────┘
         Example::new([Percentage(0), Max(20)]).render(example2, buf);
 
-        // Percentage(0), Max(40)
-        //
-        // <---------------------50 px---------------------->
-        //
-        // ┌────────┐┌──────────────────────────────────────┐
-        // │  10 px ││                 40 px                │
-        // └────────┘└──────────────────────────────────────┘
         Example::new([Percentage(0), Max(40)]).render(example3, buf);
 
-        // Percentage(0), Max(60)
-        //
-        // <---------------------50 px---------------------->
-        //
-        // ┌────────────────────────────────────────────────┐
-        // │                      50 px                     │
-        // └────────────────────────────────────────────────┘
         Example::new([Percentage(0), Max(60)]).render(example4, buf);
 
-        // Percentage(0), Max(80)
-        //
-        // <---------------------50 px---------------------->
-        //
-        // ┌────────────────────────────────────────────────┐
-        // │                      50 px                     │
-        // └────────────────────────────────────────────────┘
         Example::new([Percentage(0), Max(80)]).render(example5, buf);
     }
 }
@@ -529,10 +369,7 @@ impl Example {
 
 impl Widget for Example {
     fn render(self, area: Rect, buf: &mut Buffer) {
-        let [legend, area] = area.split(&Layout::vertical([Ratio(1, 3); 2]));
         let blocks = Layout::horizontal(&self.constraints).split(area);
-
-        self.legend(legend.width as usize).render(legend, buf);
 
         for (block, constraint) in blocks.iter().zip(&self.constraints) {
             let text = format!("{} px", block.width);
@@ -551,23 +388,6 @@ impl Widget for Example {
 }
 
 impl Example {
-    fn legend(&self, width: usize) -> Paragraph {
-        // a bar like `<----- 80 px ----->`
-        let width_label = format!("{} px", width);
-        let width_bar = format!(
-            "<{width_label:-^width$}>",
-            width = width - width_label.len() / 2
-        );
-        Paragraph::new(width_bar.dark_gray())
-            .alignment(Alignment::Center)
-            .block(Block::default().padding(Padding {
-                left: 0,
-                right: 0,
-                top: 1,
-                bottom: 0,
-            }))
-    }
-
     fn illustration(&self, constraint: Constraint, text: String, fg: Color) -> Paragraph {
         Paragraph::new(format!("{:?}", constraint))
             .alignment(Alignment::Center)
