aa27dabfd23df5bca36ee50a337cd1220758f6da
Emirhan TALA <emirhantala99ff99@gmail.com>
2024-01-17 21:53:37 +0100

    feat: update table example and table.tape
    table example now has a scrollbar
    
    colors changed to style::palette::material::AccentedPalette colors
    
    now colors can be changed with keys h, l, left arrow, right arrow
    
    added a footer for key info
    
    updated table.tape in order to have a better looking gif with new setup and changed typing speed to 0.75s from 0.5s

--- examples/table.rs
+++ examples/table.rs
@@ -1,4 +1,4 @@
-use std::{error::Error, io, str::FromStr};
+use std::{error::Error, io};
 
 use crossterm::{
     event::{self, DisableMouseCapture, EnableMouseCapture, Event, KeyCode, KeyEventKind},
@@ -7,17 +7,32 @@ use crossterm::{
 };
 use itertools::Itertools;
 use ratatui::{prelude::*, widgets::*};
+use style::palette::material;
+
+const EXAMPLE_TABLE_COLORS: [material::AccentedPalette; 4] = [
+    material::RED,
+    material::GREEN,
+    material::BLUE,
+    material::ORANGE,
+];
+const INFO_TEXT: &str =
+    "(q) to quit / (j, ↑) to go up / (k, ↓) to go down / (l, →) for next color / (h, ←) for previous color";
 
 struct App {
     state: TableState,
     items: Vec<Vec<String>>,
+    scroll_state: ScrollbarState,
+    color_index: usize,
 }
 
 impl App {
     fn new() -> App {
+        let items = generate_fake_names();
         App {
             state: TableState::default().with_selected(0),
-            items: generate_fake_names(),
+            scroll_state: ScrollbarState::new((items.len() - 1) * 4),
+            color_index: 0,
+            items,
         }
     }
     pub fn next(&mut self) {
@@ -32,6 +47,7 @@ impl App {
             None => 0,
         };
         self.state.select(Some(i));
+        self.scroll_state = self.scroll_state.position(i * 4);
     }
 
     pub fn previous(&mut self) {
@@ -46,6 +62,23 @@ impl App {
             None => 0,
         };
         self.state.select(Some(i));
+        self.scroll_state = self.scroll_state.position(i * 4);
+    }
+
+    pub fn next_color(&mut self) {
+        if self.color_index >= EXAMPLE_TABLE_COLORS.len() - 1 {
+            self.color_index = 0;
+        } else {
+            self.color_index += 1;
+        }
+    }
+
+    pub fn previous_color(&mut self) {
+        if self.color_index == 0 {
+            self.color_index = EXAMPLE_TABLE_COLORS.len() - 1;
+        } else {
+            self.color_index -= 1
+        }
     }
 }
 
@@ -107,6 +140,8 @@ fn run_app<B: Backend>(terminal: &mut Terminal<B>, mut app: App) -> io::Result<(
                     KeyCode::Char('q') => return Ok(()),
                     KeyCode::Down | KeyCode::Char('j') => app.next(),
                     KeyCode::Up | KeyCode::Char('k') => app.previous(),
+                    KeyCode::Right | KeyCode::Char('l') => app.next_color(),
+                    KeyCode::Left | KeyCode::Char('h') => app.previous_color(),
                     _ => {}
                 }
             }
@@ -115,15 +150,22 @@ fn run_app<B: Backend>(terminal: &mut Terminal<B>, mut app: App) -> io::Result<(
 }
 
 fn ui(f: &mut Frame, app: &mut App) {
-    let rects = Layout::vertical([Constraint::Percentage(100)]).split(f.size());
+    let rects = Layout::default()
+        .direction(Direction::Vertical)
+        .constraints([Constraint::Min(5), Constraint::Length(3)])
+        .split(f.size());
 
-    // colors from https://tailwindcss.com/docs/customizing-colors
-    let header_bg = Color::from_str("#1e3a8a").unwrap();
-    let header_fg = Color::from_str("#eff6ff").unwrap();
+    let curr_color = EXAMPLE_TABLE_COLORS[app.color_index];
+
+    let buffer_bg = curr_color.c900;
+    let header_bg = curr_color.c900;
+    let header_fg = material::WHITE;
     let header_style = Style::default().fg(header_fg).bg(header_bg);
-    let normal_row_color = Color::from_str("#1e293b").unwrap();
-    let alt_row_color = Color::from_str("#0f172a").unwrap();
+    let row_fg = material::GRAY.c100;
+    let normal_row_color = curr_color.c600;
+    let alt_row_color = curr_color.c400;
     let selected_style = Style::default().add_modifier(Modifier::REVERSED);
+    let footer_border_color = curr_color.c400;
 
     let header = ["Name", "Address", "Email"]
         .iter()
@@ -141,16 +183,16 @@ fn ui(f: &mut Frame, app: &mut App) {
             .cloned()
             .map(|content| Cell::from(Text::from(format!("\n{}\n", content))))
             .collect::<Row>()
-            .style(Style::new().bg(color))
+            .style(Style::new().fg(row_fg).bg(color))
             .height(4)
     });
     let bar = " █ ";
     let t = Table::new(
         rows,
         [
-            Constraint::Length(15),
+            Constraint::Length(25),
+            Constraint::Min(40),
             Constraint::Min(30),
-            Constraint::Min(25),
         ],
     )
     .header(header)
@@ -161,6 +203,29 @@ fn ui(f: &mut Frame, app: &mut App) {
         bar.into(),
         "".into(),
     ]))
+    .bg(buffer_bg)
     .highlight_spacing(HighlightSpacing::Always);
     f.render_stateful_widget(t, rects[0], &mut app.state);
+    f.render_stateful_widget(
+        Scrollbar::default()
+            .orientation(ScrollbarOrientation::VerticalRight)
+            .begin_symbol(None)
+            .end_symbol(None),
+        rects[0].inner(&Margin {
+            vertical: 1,
+            horizontal: 1,
+        }),
+        &mut app.scroll_state,
+    );
+
+    let info_footer = Paragraph::new(Line::from(INFO_TEXT))
+        .style(Style::new().fg(row_fg).bg(buffer_bg))
+        .alignment(Alignment::Center)
+        .block(
+            Block::default()
+                .borders(Borders::ALL)
+                .border_style(Style::new().fg(footer_border_color))
+                .border_type(BorderType::Double),
+        );
+    f.render_widget(info_footer, rects[1]);
 }
