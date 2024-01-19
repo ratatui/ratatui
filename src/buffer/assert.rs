9f1b8c9c681a09e0be254bdcb1fa21c953e23a14
Dheepak Krishnamurthy <me@kdheepak.com>
2024-01-18 23:37:18 -0500

    feat: Adds message to `assert_buffer_eq!` ✨

--- src/buffer/assert.rs
+++ src/buffer/assert.rs
@@ -4,6 +4,9 @@
 #[macro_export]
 macro_rules! assert_buffer_eq {
     ($actual_expr:expr, $expected_expr:expr) => {
+        assert_buffer_eq!($actual_expr, $expected_expr, "buffers not equal")
+    };
+    ($actual_expr:expr, $expected_expr:expr, $message:expr) => {
         match (&$actual_expr, &$expected_expr) {
             (actual, expected) => {
                 if actual.area != expected.area {
@@ -46,7 +49,7 @@ macro_rules! assert_buffer_eq {
                 }
                 // shouldn't get here, but this guards against future behavior
                 // that changes equality but not area or content
-                assert_eq!(actual, expected, "buffers not equal");
+                assert_eq!(actual, expected, $message);
             }
         }
     };
