8c3b43b80619dfbb37f48dd18e161eda65ff4e63
Dheepak Krishnamurthy <me@kdheepak.com>
2024-01-22 05:11:44 -0500

    test(layout): Add tests for split_with_spacing 🚨

--- src/layout/layout.rs
+++ src/layout/layout.rs
@@ -2210,12 +2210,12 @@ mod tests {
         #[case::fixed_spacing(vec![(10 , 20), (40, 20) , (70, 20)], vec![Fixed(20) , Fixed(20) , Fixed(20)] , Flex::SpaceAround, 2)]
         fn flex_spacing(
             #[case] expected: Vec<(u16, u16)>,
-            #[case] lengths: Vec<Constraint>,
+            #[case] constraints: Vec<Constraint>,
             #[case] flex: Flex,
             #[case] spacing: u16,
         ) {
             let rect = Rect::new(0, 0, 100, 1);
-            let r = Layout::horizontal(lengths)
+            let r = Layout::horizontal(constraints)
                 .flex(flex)
                 .spacing(spacing)
                 .split(rect);
@@ -2299,11 +2299,11 @@ mod tests {
         #[case::flex(vec![(40 , 10), (50, 10)] , vec![Fixed(10), Fixed(10)], Flex::Center)]
         fn proportional_vs_flex(
             #[case] expected: Vec<(u16, u16)>,
-            #[case] lengths: Vec<Constraint>,
+            #[case] constraints: Vec<Constraint>,
             #[case] flex: Flex,
         ) {
             let rect = Rect::new(0, 0, 100, 1);
-            let r = Layout::horizontal(lengths).flex(flex).split(rect);
+            let r = Layout::horizontal(constraints).flex(flex).split(rect);
             let result = r
                 .iter()
                 .map(|r| (r.x, r.width))
@@ -2344,12 +2344,12 @@ mod tests {
         #[case::flex_fixed10(vec![(0 , 45), (45, 10), (55 , 45)] , vec![Proportional(1), Fixed(10), Proportional(1)], Flex::SpaceBetween , 10)]
         fn proportional_spacing(
             #[case] expected: Vec<(u16, u16)>,
-            #[case] lengths: Vec<Constraint>,
+            #[case] constraints: Vec<Constraint>,
             #[case] flex: Flex,
             #[case] spacing: u16,
         ) {
             let rect = Rect::new(0, 0, 100, 1);
-            let r = Layout::horizontal(lengths)
+            let r = Layout::horizontal(constraints)
                 .flex(flex)
                 .spacing(spacing)
                 .split(rect);
@@ -2364,16 +2364,95 @@ mod tests {
         #[case::flex_fixed10(vec![(0, 10), (90, 10)], vec![Length(10), Length(10)], Flex::Center, 80)]
         fn flex_spacing_lower_priority_than_user_spacing(
             #[case] expected: Vec<(u16, u16)>,
-            #[case] lengths: Vec<Constraint>,
+            #[case] constraints: Vec<Constraint>,
             #[case] flex: Flex,
             #[case] spacing: u16,
         ) {
             let rect = Rect::new(0, 0, 100, 1);
-            let r = Layout::horizontal(lengths)
+            let r = Layout::horizontal(constraints)
                 .flex(flex)
                 .spacing(spacing)
                 .split(rect);
             let result = r
+                .iter()
+                .map(|r| (r.x, r.width))
+                .collect::<Vec<(u16, u16)>>();
+            assert_eq!(expected, result);
+        }
+
+        #[rstest]
+        #[case::spacers(vec![(0, 0), (10, 0), (100, 0)], vec![Length(10), Length(10)], Flex::StretchLast)]
+        #[case::spacers(vec![(0, 0), (50, 0), (100, 0)], vec![Length(10), Length(10)], Flex::Stretch)]
+        #[case::spacers(vec![(0, 0), (10, 80), (100, 0)], vec![Length(10), Length(10)], Flex::SpaceBetween)]
+        #[case::spacers(vec![(0, 27), (37, 26), (73, 27)], vec![Length(10), Length(10)], Flex::SpaceAround)]
+        #[case::spacers(vec![(0, 0), (10, 0), (20, 80)], vec![Length(10), Length(10)], Flex::Start)]
+        #[case::spacers(vec![(0, 40), (50, 0), (60, 40)], vec![Length(10), Length(10)], Flex::Center)]
+        #[case::spacers(vec![(0, 80), (90, 0), (100, 0)], vec![Length(10), Length(10)], Flex::End)]
+        fn split_with_spacers_no_spacing(
+            #[case] expected: Vec<(u16, u16)>,
+            #[case] constraints: Vec<Constraint>,
+            #[case] flex: Flex,
+        ) {
+            let rect = Rect::new(0, 0, 100, 1);
+            let (_, s) = Layout::horizontal(&constraints)
+                .flex(flex)
+                .split_with_spacers(rect);
+            assert_eq!(s.len(), constraints.len() + 1);
+            let result = s
+                .iter()
+                .map(|r| (r.x, r.width))
+                .collect::<Vec<(u16, u16)>>();
+            assert_eq!(expected, result);
+        }
+
+        #[rstest]
+        #[case::spacers(vec![(0, 0), (10, 5), (100, 0)], vec![Length(10), Length(10)], Flex::StretchLast, 5)]
+        #[case::spacers(vec![(0, 0), (48, 5), (100, 0)], vec![Length(10), Length(10)], Flex::Stretch, 5)]
+        #[case::spacers(vec![(0, 0), (10, 80), (100, 0)], vec![Length(10), Length(10)], Flex::SpaceBetween, 5)]
+        #[case::spacers(vec![(0, 27), (37, 26), (73, 27)], vec![Length(10), Length(10)], Flex::SpaceAround, 5)]
+        #[case::spacers(vec![(0, 0), (10, 5), (25, 75)], vec![Length(10), Length(10)], Flex::Start, 5)]
+        #[case::spacers(vec![(0, 38), (48, 5), (63, 37)], vec![Length(10), Length(10)], Flex::Center, 5)]
+        #[case::spacers(vec![(0, 75), (85, 5), (100, 0)], vec![Length(10), Length(10)], Flex::End, 5)]
+        fn split_with_spacers_and_spacing(
+            #[case] expected: Vec<(u16, u16)>,
+            #[case] constraints: Vec<Constraint>,
+            #[case] flex: Flex,
+            #[case] spacing: u16,
+        ) {
+            let rect = Rect::new(0, 0, 100, 1);
+            let (_, s) = Layout::horizontal(&constraints)
+                .flex(flex)
+                .spacing(spacing)
+                .split_with_spacers(rect);
+            assert_eq!(s.len(), constraints.len() + 1);
+            let result = s
+                .iter()
+                .map(|r| (r.x, r.width))
+                .collect::<Vec<(u16, u16)>>();
+            assert_eq!(expected, result);
+        }
+
+        #[rstest]
+        #[case::spacers(vec![(0, 0), (0, 100), (100, 0)], vec![Length(10), Length(10)], Flex::StretchLast, 200)]
+        #[case::spacers(vec![(0, 0), (0, 100), (100, 0)], vec![Length(10), Length(10)], Flex::Stretch, 200)]
+        #[case::spacers(vec![(0, 0), (10, 80), (100, 0)], vec![Length(10), Length(10)], Flex::SpaceBetween, 200)]
+        #[case::spacers(vec![(0, 27), (37, 26), (73, 27)], vec![Length(10), Length(10)], Flex::SpaceAround, 200)]
+        #[case::spacers(vec![(0, 0), (0, 100), (100, 0)], vec![Length(10), Length(10)], Flex::Start, 200)]
+        #[case::spacers(vec![(0, 0), (0, 100), (100, 0)], vec![Length(10), Length(10)], Flex::Center, 200)]
+        #[case::spacers(vec![(0, 0), (0, 100), (100, 0)], vec![Length(10), Length(10)], Flex::End, 200)]
+        fn split_with_spacers_and_too_much_spacing(
+            #[case] expected: Vec<(u16, u16)>,
+            #[case] constraints: Vec<Constraint>,
+            #[case] flex: Flex,
+            #[case] spacing: u16,
+        ) {
+            let rect = Rect::new(0, 0, 100, 1);
+            let (_, s) = Layout::horizontal(&constraints)
+                .flex(flex)
+                .spacing(spacing)
+                .split_with_spacers(rect);
+            assert_eq!(s.len(), constraints.len() + 1);
+            let result = s
                 .iter()
                 .map(|r| (r.x, r.width))
                 .collect::<Vec<(u16, u16)>>();
