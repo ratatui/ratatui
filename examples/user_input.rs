ab523e4f172226a347e4b6672969c3bd867f2cd3
Josh McKinney <joshka@users.noreply.github.com>
2024-01-24 11:28:05 -0800

    docs(examples): add note about example versions to all examples

--- examples/user_input.rs
+++ examples/user_input.rs
@@ -1,19 +1,34 @@
+//! # [Ratatui] User Input example
+//!
+//! The latest version of this example is available in the [examples] folder in the repository.
+//!
+//! Please note that the examples are designed to be run against the `main` branch of the Github
+//! repository. This means that you may not be able to compile with the latest release version on
+//! crates.io, or the one that you have installed locally.
+//!
+//! See the [examples readme] for more information on finding examples that match the version of the
+//! library you are using.
+//!
+//! [Ratatui]: https://github.com/ratatui-org/ratatui
+//! [examples]: https://github.com/ratatui-org/ratatui/blob/main/examples
+//! [examples readme]: https://github.com/ratatui-org/ratatui/blob/main/examples/README.md
+
 use std::{error::Error, io};
 
-/// A simple example demonstrating how to handle user input. This is
-/// a bit out of the scope of the library as it does not provide any
-/// input handling out of the box. However, it may helps some to get
-/// started.
+/// A simple example demonstrating how to handle user input. This is a bit out of the scope of
+/// the library as it does not provide any input handling out of the box. However, it may helps
+/// some to get started.
 ///
 /// This is a very simple example:
-///   * An input box always focused. Every character you type is registered
-///   here.
+///   * An input box always focused. Every character you type is registered here.
 ///   * An entered character is inserted at the cursor position.
 ///   * Pressing Backspace erases the left character before the cursor position
-///   * Pressing Enter pushes the current input in the history of previous
-///   messages.
-/// **Note: ** as this is a relatively simple example unicode characters are unsupported and
-/// their use will result in undefined behaviour.
+///   * Pressing Enter pushes the current input in the history of previous messages. **Note: **
+///     as
+///   this is a relatively simple example unicode characters are unsupported and their use will
+/// result in undefined behaviour.
+///
+/// See also https://github.com/rhysd/tui-textarea and https://github.com/sayanarijit/tui-input/
 use crossterm::{
     event::{self, DisableMouseCapture, EnableMouseCapture, Event, KeyCode, KeyEventKind},
     execute,
