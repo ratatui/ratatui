c2708fb750b50fe73c2e3fa603336ec637f36564
Stephen Brown <december1981@gmail.com>
2024-02-17 16:33:45 +0000

    pass by value

--- src/widgets/paragraph.rs
+++ src/widgets/paragraph.rs
@@ -339,7 +339,7 @@ impl Paragraph<'_> {
     /// Visits the styled wrapped text lines inside a text area for rendering or other analysis/processing.
     /// The visitor function indicates it wants the visitor iteration to terminate if it returns false.
     ///
-    pub fn visit_wrapped_text<F: FnMut(&'_ WrappedLine) -> bool>(
+    pub fn visit_wrapped_text<F: FnMut(WrappedLine) -> bool>(
         &self,
         text_area: &Rect,
         visitor: F,
@@ -364,12 +364,12 @@ impl Paragraph<'_> {
         }
     }
 
-    fn visit_with_composer<'a, C: LineComposer<'a>, F: FnMut(&'_ WrappedLine) -> bool>(
+    fn visit_with_composer<'a, C: LineComposer<'a>, F: FnMut(WrappedLine) -> bool>(
         mut composer: C,
         mut visitor: F,
     ) {
         while let Some(line) = composer.next_line() {
-            if !visitor(&line) {
+            if !visitor(line) {
                 break;
             }
         }
