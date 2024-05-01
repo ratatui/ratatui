9d1e13a7ddf6fe882ff73a96f3852bef69653e08
may h <mhoque95@gmail.com>
2024-04-30 22:10:56 -0400

    docs: add note about scrollbar state content length

--- src/widgets/scrollbar.rs
+++ src/widgets/scrollbar.rs
@@ -29,6 +29,11 @@ use crate::{prelude::*, symbols::scrollbar::*};
 /// └─────────── begin
 /// ```
 ///
+/// ## Important
+/// When rendering `Scrollbar`, you must specify the `content_length`
+/// field in the [`ScrollbarState`], or else the `Scrollbar` will not render. See
+/// the documentation for [`ScrollbarState`] for more information.
+///
 /// # Examples
 ///
 /// ```rust
