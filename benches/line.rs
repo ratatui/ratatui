71da6813e724f9cbcb69403779764669aa9bd1aa
EdJoPaTo <rfc-conform-git-commit-email@funny-long-domain-label-everyone-hates-as-it-is-too-long.edjopato.de>
2024-05-10 17:03:44 +0200

    test: benchmark all alignments

--- benches/line.rs
+++ benches/line.rs
@@ -1,31 +1,38 @@
 use std::hint::black_box;
 
 use criterion::{criterion_group, criterion_main, Criterion};
-use ratatui::{buffer::Buffer, layout::Rect, style::Stylize, text::Line, widgets::Widget};
+use ratatui::{
+    buffer::Buffer,
+    layout::{Alignment, Rect},
+    style::Stylize,
+    text::Line,
+    widgets::Widget,
+};
 
 fn line_render(criterion: &mut Criterion) {
-    let mut group = criterion.benchmark_group("line_render");
-    group.sample_size(1000);
+    for alignment in [Alignment::Left, Alignment::Center, Alignment::Right] {
+        let mut group = criterion.benchmark_group(format!("line_render/{alignment}"));
+        group.sample_size(1000);
 
-    let line = &Line::from(vec![
-        "This".red(),
-        " ".green(),
-        "is".italic(),
-        " ".blue(),
-        "SPARTA!!".bold(),
-    ])
-    .right_aligned();
+        let line = &Line::from(vec![
+            "This".red(),
+            " ".green(),
+            "is".italic(),
+            " ".blue(),
+            "SPARTA!!".bold(),
+        ])
+        .alignment(alignment);
 
-    for width in [0, 3, 4, 6, 7, 10, 42] {
-        let area = Rect::new(0, 0, width, 1);
+        for width in [0, 3, 4, 6, 7, 10, 42] {
+            let area = Rect::new(0, 0, width, 1);
 
-        group.bench_function(width.to_string(), |bencher| {
-            let mut buffer = Buffer::empty(area);
-            bencher.iter(|| black_box(line).render(area, &mut buffer));
-        });
+            group.bench_function(width.to_string(), |bencher| {
+                let mut buffer = Buffer::empty(area);
+                bencher.iter(|| black_box(line).render(area, &mut buffer));
+            });
+        }
+        group.finish();
     }
-
-    group.finish();
 }
 
 criterion_group!(benches, line_render);
