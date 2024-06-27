0ea124b002f824737204441f55eafa69ea35a937
Leo Henches <leo.henches@gmail.com>
2024-06-27 12:35:12 +0200

    docs(block): moved functions from setter to constructor

--- src/widgets/block.rs
+++ src/widgets/block.rs
@@ -38,15 +38,15 @@ pub use title::{Position, Title};
 /// # Constructor methods
 ///
 /// - [`Block::new`] creates a new [`Block`] with no border or paddings.
+/// - [`Block::border_style`] Defines the style of the borders.
+/// - [`Block::border_type`] Sets the symbols used to display the border (e.g. single line, double
+///   line, thick or rounded borders).
+/// - [`Block::bordered`] Create a new block with all borders shown.
 ///
 /// # Setter methods
 ///
 /// These methods are fluent setters. They return a new [`Block`] with the specified property set.
 ///
-/// - [`Block::border_style`] Defines the style of the borders.
-/// - [`Block::border_type`] Sets the symbols used to display the border (e.g. single line, double
-///   line, thick or rounded borders).
-/// - [`Block::bordered`] Create a new block with all borders shown.
 /// - [`Block::borders`] Defines which borders to display.
 /// - [`Block::padding`] Defines the padding inside a [`Block`].
 /// - [`Block::style`] Sets the base style of the widget.
