3d3ff8d493991bbdbcfb8bc89d05304530d3877f
Dheepak Krishnamurthy <1813121+kdheepak@users.noreply.github.com>
2024-10-03 05:06:44 -0400

    chore: Better debug_segments function

--- src/layout/layout.rs
+++ src/layout/layout.rs
@@ -707,6 +707,7 @@ impl Layout {
         // `solver.fetch_changes()` can only be called once per solve
         let changes: HashMap<Variable, f64> = solver.fetch_changes().iter().copied().collect();
         // debug_segments(&segments, &changes);
+        // debug_segments(&spacers, &changes);
 
         let segment_rects = changes_to_rects(&changes, &segments, inner_area, self.direction);
         let spacer_rects = changes_to_rects(&changes, &spacers, inner_area, self.direction);
@@ -982,14 +983,17 @@ fn changes_to_rects(
 /// layout code - we should replace this with tracing in the future.
 #[allow(dead_code)]
 fn debug_segments(segments: &[Element], changes: &HashMap<Variable, f64>) {
-    let ends = format!(
+    let variables = format!(
         "{:?}",
         segments
             .iter()
-            .map(|e| changes.get(&e.end).unwrap_or(&0.0))
-            .collect::<Vec<&f64>>()
+            .map(|e| (
+                changes.get(&e.start).unwrap_or(&0.0) / FLOAT_PRECISION_MULTIPLIER,
+                changes.get(&e.end).unwrap_or(&0.0) / FLOAT_PRECISION_MULTIPLIER,
+            ))
+            .collect::<Vec<(f64, f64)>>()
     );
-    dbg!(ends);
+    dbg!(variables);
 }
 
 /// A container used by the solver inside split
