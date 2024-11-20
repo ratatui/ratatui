15b453549348e6e80d88206afacedc87e07b95b2
Josh McKinney <joshka@users.noreply.github.com>
2024-11-19 17:14:25 -0800

    Fix lints, tweak tests

--- ratatui/tests/stateful_widget_ref_dyn.rs
+++ ratatui/tests/stateful_widget_ref_dyn.rs
@@ -1,15 +1,18 @@
 #![cfg(feature = "unstable-widget-ref")]
 
+use std::{
+    any::{type_name, Any},
+    cell::RefCell,
+};
+
+use pretty_assertions::assert_eq;
 use ratatui::widgets::StatefulWidgetRef;
-use ratatui_core::buffer::Buffer;
-use ratatui_core::layout::Rect;
-use ratatui_core::text::Line;
-use ratatui_core::widgets::Widget;
-use std::any::{type_name_of_val, Any};
-use std::cell::RefCell;
+use ratatui_core::{buffer::Buffer, layout::Rect, text::Line, widgets::Widget};
 
 trait AnyWindow: StatefulWidgetRef<State = dyn Any> {
-    fn type_name(&self) -> &str;
+    fn title(&self) -> &str {
+        type_name::<Self>()
+    }
 }
 
 struct Window1;
@@ -18,18 +21,14 @@ struct Window1State {
     pub value: u32,
 }
 
-impl AnyWindow for Window1 {
-    fn type_name(&self) -> &str {
-        type_name_of_val(&self)
-    }
-}
+impl AnyWindow for Window1 {}
 
 impl StatefulWidgetRef for Window1 {
     type State = dyn Any;
 
     fn render_ref(&self, area: Rect, buf: &mut Buffer, state: &mut Self::State) {
         let state = state.downcast_mut::<Window1State>().expect("window1 state");
-        Line::from(format!("u32: {}", state.value)).render(area, buf);
+        Line::from(format!("{}, u32: {}", self.title(), state.value)).render(area, buf);
     }
 }
 
@@ -39,60 +38,54 @@ struct Window2State {
     pub value: String,
 }
 
-impl AnyWindow for Window2 {
-    fn type_name(&self) -> &str {
-        type_name_of_val(&self)
-    }
-}
+impl AnyWindow for Window2 {}
 
 impl StatefulWidgetRef for Window2 {
     type State = dyn Any;
 
     fn render_ref(&self, area: Rect, buf: &mut Buffer, state: &mut Self::State) {
         let state = state.downcast_mut::<Window2State>().expect("window2 state");
-        Line::from(format!("String: {}", state.value)).render(area, buf);
+        Line::from(format!("{}, String: {}", self.title(), state.value)).render(area, buf);
     }
 }
 
+type BoxedWindow = Box<dyn AnyWindow>;
+type BoxedState = Box<RefCell<dyn Any>>;
+
 #[test]
 fn render_dyn_widgets() {
-    let mut windows: Vec<(Box<dyn AnyWindow>, Box<RefCell<dyn Any>>)> = Vec::new();
+    let windows: Vec<(BoxedWindow, BoxedState)> = vec![
+        (
+            Box::new(Window1),
+            Box::new(RefCell::new(Window1State { value: 32 })),
+        ),
+        (
+            Box::new(Window2),
+            Box::new(RefCell::new(Window2State {
+                value: "Some".to_string(),
+            })),
+        ),
+        (
+            Box::new(Window1),
+            Box::new(RefCell::new(Window1State { value: 42 })),
+        ),
+    ];
 
-    windows.push((
-        Box::new(Window1),
-        Box::new(RefCell::new(Window1State { value: 32 })),
-    ));
-    windows.push((
-        Box::new(Window2),
-        Box::new(RefCell::new(Window2State {
-            value: "Some".to_string(),
-        })),
-    ));
-    windows.push((
-        Box::new(Window1),
-        Box::new(RefCell::new(Window1State { value: 42 })),
-    ));
+    let mut buf = Buffer::empty(Rect::new(0, 0, 50, 3));
 
-    let mut buf = Buffer::empty(Rect::new(0, 0, 20, 5));
-
-    let mut area = Rect::new(0, 0, 20, 1);
-    for (w, s) in windows.iter() {
-        eprintln!("render {}", w.type_name());
+    let mut area = Rect::new(0, 0, 50, 1);
+    for (w, s) in &windows {
         let mut s = s.borrow_mut();
         w.render_ref(area, &mut buf, &mut *s);
         area.y += 1;
     }
 
-    let buf_string = buf
-        .content
-        .iter()
-        .map(|v| v.symbol())
-        .fold(String::new(), |mut v, w| {
-            v.push_str(w);
-            v
-        });
     assert_eq!(
-        buf_string,
-        "u32: 32             String: Some        u32: 42                                                     "
+        buf,
+        Buffer::with_lines([
+            "stateful_widget_ref_dyn::Window1, u32: 32         ",
+            "stateful_widget_ref_dyn::Window2, String: Some    ",
+            "stateful_widget_ref_dyn::Window1, u32: 42         ",
+        ])
     );
 }
