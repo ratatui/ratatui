453ed496c0430a7d95abfe27e65487b513ab371a
Raffaele Cataldo <kemanicataldo@gmail.com>
2024-11-29 08:09:28 -0800

    fix(scrollbar): check for area.is_empty() before rendering to prevent panicking and add tests, fix typo scollbar -> scrollbar

--- ratatui-widgets/src/scrollbar.rs
+++ ratatui-widgets/src/scrollbar.rs
@@ -500,12 +500,15 @@ impl StatefulWidget for Scrollbar<'_> {
     type State = ScrollbarState;
 
     fn render(self, area: Rect, buf: &mut Buffer, state: &mut Self::State) {
-        if state.content_length == 0 || self.track_length_excluding_arrow_heads(area) == 0 {
+        if state.content_length == 0
+            || area.is_empty()
+            || self.track_length_excluding_arrow_heads(area) == 0
+        {
             return;
         }
 
         let mut bar = self.bar_symbols(area, state);
-        let area = self.scollbar_area(area);
+        let area = self.scrollbar_area(area);
         for x in area.left()..area.right() {
             for y in area.top()..area.bottom() {
                 if let Some(Some((symbol, style))) = bar.next() {
@@ -581,7 +584,7 @@ impl Scrollbar<'_> {
         (thumb_start, thumb_length, track_end_length)
     }
 
-    fn scollbar_area(&self, area: Rect) -> Rect {
+    fn scrollbar_area(&self, area: Rect) -> Rect {
         match self.orientation {
             ScrollbarOrientation::VerticalLeft => area.columns().next(),
             ScrollbarOrientation::VerticalRight => area.columns().last(),
@@ -1064,5 +1067,21 @@ mod tests {
             .viewport_content_length(2);
         scrollbar_no_arrows.render(buffer.area, &mut buffer, &mut state);
         assert_eq!(buffer, Buffer::with_lines([expected]));
+    }
+
+    #[rstest]
+    #[case::scrollbar_height_0(10, 0)]
+    #[case::scrollbar_width_0(0, 10)]
+    fn do_not_render_with_empty_area(#[case] width: u16, #[case] height: u16) {
+        let scrollbar = Scrollbar::new(ScrollbarOrientation::VerticalRight)
+            .begin_symbol(Some("<"))
+            .end_symbol(Some(">"))
+            .track_symbol(Some("-"))
+            .thumb_symbol("#");
+        let zero_width_area = Rect::new(0, 0, width, height);
+        let mut buffer = Buffer::empty(Rect::new(0, 0, 10, 10));
+
+        let mut state = ScrollbarState::new(10);
+        scrollbar.render(zero_width_area, &mut buffer, &mut state);
     }
 }
