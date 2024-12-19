52e60c8002fa0b969ff4abb61b8c0f0c528a5518
Josh McKinney <joshka@users.noreply.github.com>
2024-12-19 14:37:04 -0800

    docs: move colors-rgb example to examples folder
    Part of #1512

---
+++ examples/apps/colors-rgb/README.md
@@ -0,0 +1,9 @@
+# Colors-RGB demo
+
+This example shows the full range of RGB colors in an animation.
+
+To run this demo:
+
+```shell
+cargo run -p colors-rgb
+```
--- examples/apps/colors-rgb/src/main.rs
+++ examples/apps/colors-rgb/src/main.rs
@@ -1,30 +1,20 @@
-//! # [Ratatui] `Colors_RGB` example
+//! A Ratatui example that shows the full range of RGB colors that can be displayed in the terminal.
 //!
-//! The latest version of this example is available in the [examples] folder in the repository.
+//! Requires a terminal that supports 24-bit color (true color) and unicode.
 //!
-//! Please note that the examples are designed to be run against the `main` branch of the Github
-//! repository. This means that you may not be able to compile with the latest release version on
-//! crates.io, or the one that you have installed locally.
+//! This example also demonstrates how implementing the Widget trait on a mutable reference
+//! allows the widget to update its state while it is being rendered. This allows the fps
+//! widget to update the fps calculation and the colors widget to update a cached version of
+//! the colors to render instead of recalculating them every frame.
 //!
-//! See the [examples readme] for more information on finding examples that match the version of the
-//! library you are using.
+//! This is an alternative to using the `StatefulWidget` trait and a separate state struct. It
+//! is useful when the state is only used by the widget and doesn't need to be shared with
+//! other widgets.
 //!
-//! [Ratatui]: https://github.com/ratatui/ratatui
-//! [examples]: https://github.com/ratatui/ratatui/blob/main/examples
-//! [examples readme]: https://github.com/ratatui/ratatui/blob/main/examples/README.md
-
-// This example shows the full range of RGB colors that can be displayed in the terminal.
-//
-// Requires a terminal that supports 24-bit color (true color) and unicode.
-//
-// This example also demonstrates how implementing the Widget trait on a mutable reference
-// allows the widget to update its state while it is being rendered. This allows the fps
-// widget to update the fps calculation and the colors widget to update a cached version of
-// the colors to render instead of recalculating them every frame.
-//
-// This is an alternative to using the `StatefulWidget` trait and a separate state struct. It
-// is useful when the state is only used by the widget and doesn't need to be shared with
-// other widgets.
+//! This example runs with the Ratatui library code in the branch that you are currently reading.
+//! See the [`latest`] branch for the code which works with the most recent Ratatui release.
+//!
+//! [`latest`]: https://github.com/ratatui/ratatui/tree/latest
 
 use std::time::{Duration, Instant};
 
