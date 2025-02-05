2e1dcb004e9101a7d1b42acc6e09de187dbfa8a7
Orhun Parmaksız <orhunparmaksiz@gmail.com>
2025-02-06 00:28:38 +0300

    docs(examples): move user-input example to examples folder

--- examples/apps/user-input/src/main.rs
+++ examples/apps/user-input/src/main.rs
@@ -1,32 +1,25 @@
-//! # [Ratatui] User Input example
-//!
-//! The latest version of this example is available in the [examples] folder in the repository.
-//!
-//! Please note that the examples are designed to be run against the `main` branch of the Github
-//! repository. This means that you may not be able to compile with the latest release version on
-//! crates.io, or the one that you have installed locally.
-//!
-//! See the [examples readme] for more information on finding examples that match the version of the
-//! library you are using.
-//!
-//! [Ratatui]: https://github.com/ratatui/ratatui
-//! [examples]: https://github.com/ratatui/ratatui/blob/main/examples
-//! [examples readme]: https://github.com/ratatui/ratatui/blob/main/examples/README.md
-
-// A simple example demonstrating how to handle user input. This is a bit out of the scope of
-// the library as it does not provide any input handling out of the box. However, it may helps
-// some to get started.
-//
-// This is a very simple example:
-//   * An input box always focused. Every character you type is registered here.
-//   * An entered character is inserted at the cursor position.
-//   * Pressing Backspace erases the left character before the cursor position
-//   * Pressing Enter pushes the current input in the history of previous messages. **Note: ** as
-//   this is a relatively simple example unicode characters are unsupported and their use will
-// result in undefined behaviour.
-//
-// See also https://github.com/rhysd/tui-textarea and https://github.com/sayanarijit/tui-input/
-
+/// A simple example demonstrating how to handle user input.
+///
+/// This is a bit out of the scope of
+/// the library as it does not provide any input handling out of the box. However, it may helps
+/// some to get started.
+///
+/// This is a very simple example:
+///   * An input box always focused. Every character you type is registered here.
+///   * An entered character is inserted at the cursor position.
+///   * Pressing Backspace erases the left character before the cursor position
+///   * Pressing Enter pushes the current input in the history of previous messages.
+///
+/// **Note:** as this is a relatively simple example unicode characters are unsupported and
+/// their use will result in undefined behaviour.
+///
+/// See also <https://github.com/rhysd/tui-textarea> and <https://github.com/sayanarijit/tui-input>/
+///
+/// This example runs with the Ratatui library code in the branch that you are currently
+/// reading. See the [`latest`] branch for the code which works with the most recent Ratatui
+/// release.
+///
+/// [`latest`]: https://github.com/ratatui/ratatui/tree/latest
 use color_eyre::Result;
 use ratatui::{
     crossterm::event::{self, Event, KeyCode, KeyEventKind},
