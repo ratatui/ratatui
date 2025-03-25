5351f68f3805fee1d4cae8d7972a25d14ee3c455
pauladam94 <poladam2002@gmail.com>
2025-03-25 09:12:55 +0100

    clippy fix

--- ratatui-core/src/symbols/merge.rs
+++ ratatui-core/src/symbols/merge.rs
@@ -1,6 +1,6 @@
-use crate::symbols::line::*;
+use crate::symbols::line::{BorderSymbol, LineStyle};
 
-pub fn merge_border(prev: BorderSymbol, next: BorderSymbol) -> BorderSymbol {
+pub const fn merge_border(prev: BorderSymbol, next: BorderSymbol) -> BorderSymbol {
     let mut res = BorderSymbol::new(None, None, None, None);
 
     res.right = merge_line_style(prev.right, next.right);
@@ -11,17 +11,18 @@ pub fn merge_border(prev: BorderSymbol, next: BorderSymbol) -> BorderSymbol {
     res
 }
 
-pub fn merge_line_style(prev: Option<LineStyle>, next: Option<LineStyle>) -> Option<LineStyle> {
+pub const fn merge_line_style(
+    prev: Option<LineStyle>,
+    next: Option<LineStyle>,
+) -> Option<LineStyle> {
     match (prev, next) {
         (None, None) => None,
         (Some(s), None) | (None, Some(s)) => Some(s),
-        (Some(LineStyle::Thick), Some(LineStyle::Normal))
+        (Some(LineStyle::Thick), Some(LineStyle::Normal | LineStyle::Thick))
         | (Some(LineStyle::Normal), Some(LineStyle::Thick)) => Some(LineStyle::Thick),
-        (Some(LineStyle::Double), Some(LineStyle::Normal))
+        (Some(LineStyle::Double), Some(LineStyle::Normal | LineStyle::Double))
         | (Some(LineStyle::Normal), Some(LineStyle::Double)) => Some(LineStyle::Double),
         (Some(LineStyle::Normal), Some(LineStyle::Normal)) => Some(LineStyle::Normal),
-        (Some(LineStyle::Thick), Some(LineStyle::Thick)) => Some(LineStyle::Thick),
-        (Some(LineStyle::Double), Some(LineStyle::Double)) => Some(LineStyle::Double),
         (Some(LineStyle::DoubleDash), Some(LineStyle::DoubleDash)) => Some(LineStyle::DoubleDash),
         (Some(LineStyle::TripleDash), Some(LineStyle::TripleDash)) => Some(LineStyle::TripleDash),
         (Some(LineStyle::QuadrupleDash), Some(LineStyle::QuadrupleDash)) => {
