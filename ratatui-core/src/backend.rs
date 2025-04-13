fd8e4907b612f8c80a4b8abe0448e21d39b4c75c
Christian Göttsche <cgzones@googlemail.com>
2025-04-13 16:49:14 +0200

    fix: avoid unnecessary imports in minimal build
    core::ops::Range is only used with the feature `scrolling-regions`.
    Ensure a minimal `cargo check` reports no warnings.

--- ratatui-core/src/backend.rs
+++ ratatui-core/src/backend.rs
@@ -100,7 +100,6 @@
 //! [Examples]: https://github.com/ratatui/ratatui/tree/main/ratatui/examples/README.md
 //! [Backend Comparison]: https://ratatui.rs/concepts/backends/comparison/
 //! [Ratatui Website]: https://ratatui.rs
-use core::ops;
 use std::io;
 
 use strum::{Display, EnumString};
@@ -342,7 +341,11 @@ pub trait Backend {
     /// For examples of how this function is expected to work, refer to the tests for
     /// [`TestBackend::scroll_region_up`].
     #[cfg(feature = "scrolling-regions")]
-    fn scroll_region_up(&mut self, region: ops::Range<u16>, line_count: u16) -> io::Result<()>;
+    fn scroll_region_up(
+        &mut self,
+        region: core::ops::Range<u16>,
+        line_count: u16,
+    ) -> io::Result<()>;
 
     /// Scroll a region of the screen downwards, where a region is specified by a (half-open) range
     /// of rows.
@@ -363,7 +366,11 @@ pub trait Backend {
     /// For examples of how this function is expected to work, refer to the tests for
     /// [`TestBackend::scroll_region_down`].
     #[cfg(feature = "scrolling-regions")]
-    fn scroll_region_down(&mut self, region: ops::Range<u16>, line_count: u16) -> io::Result<()>;
+    fn scroll_region_down(
+        &mut self,
+        region: core::ops::Range<u16>,
+        line_count: u16,
+    ) -> io::Result<()>;
 }
 
 #[cfg(test)]
