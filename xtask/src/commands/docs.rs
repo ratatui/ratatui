4a54ac477ed53fb7ca007ad0c3fadd255fb2a882
Josh McKinney <joshka@users.noreply.github.com>
2025-07-14 23:06:05 -0700

    chore: use cargo-hack for cargo xtask docs
    --open flag now opens all docs rather than just the first doc

--- xtask/src/commands/docs.rs
+++ xtask/src/commands/docs.rs
@@ -1,7 +1,6 @@
 use color_eyre::Result;
-use itertools::{Itertools, Position};
 
-use crate::{Run, run_cargo_nightly, workspace_libs};
+use crate::{Run, run_cargo_nightly};
 
 /// Check documentation for errors and warnings
 #[derive(Clone, Debug, clap::Args)]
@@ -13,14 +12,11 @@ pub struct Docs {
 
 impl Run for Docs {
     fn run(self) -> Result<()> {
-        let packages = workspace_libs()?;
-        for (position, package) in packages.iter().with_position() {
-            let mut args = vec!["docs-rs", "--package", &package];
-            if self.open && matches!(position, Position::Last | Position::Only) {
-                args.push("--open");
-            }
-            run_cargo_nightly(args)?;
+        // cargo +nightly hack --all --ignore-private docs-rs
+        let mut args = vec!["hack", "--all", "--ignore-private", "docs-rs"];
+        if self.open {
+            args.push("--open");
         }
-        Ok(())
+        run_cargo_nightly(args)
     }
 }
