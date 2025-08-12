e59f5831272fb07c9d1f23854ad15778b2ccf44d
kashregister <58663785+kashregister@users.noreply.github.com>
2025-08-12 13:29:00 +0200

    refactor: addressed changes

--- ratatui/benches/main/constraints.rs
+++ ratatui/benches/main/constraints.rs
@@ -1,59 +1,60 @@
 use std::hint::black_box;
 use std::rc::Rc;
 
-use criterion::{Criterion, criterion_group};
+use criterion::{criterion_group, Criterion};
 use ratatui::layout::Constraint::{Fill, Length, Max, Min, Percentage, Ratio};
-use ratatui::layout::{Direction, Layout, Rect};
+use ratatui::layout::{Layout, Rect};
 
 const SPLIT_BY: u16 = 10;
 
-fn generate_layout(constraint: &str, area: Rect) -> Rc<[Rect]> {
-    Layout::default()
-        .direction(Direction::Vertical)
-        .constraints({
-            let mut cs = Vec::new();
-            for _ in 0..SPLIT_BY {
-                match constraint {
-                    "Fill" => {
-                        cs.push(Fill(1));
-                    }
-                    "Length" => {
-                        cs.push(Length(area.width / SPLIT_BY));
-                    }
-                    "Max" => {
-                        cs.push(Max(area.width / SPLIT_BY));
-                    }
-                    "Min" => {
-                        cs.push(Min(area.width / SPLIT_BY));
-                    }
-                    "Percentage" => {
-                        cs.push(Percentage(100 / SPLIT_BY));
-                    }
-                    "Ratio" => {
-                        cs.push(Ratio(1, SPLIT_BY.into()));
-                    }
-                    _ => todo!(),
-                }
-            }
-            cs
-        })
-        .split(area)
-}
-
-fn constraints_render(criterion: &mut Criterion) {
-    for size in [16, 64, 255] {
+fn layout_split(criterion: &mut Criterion) {
+    for size in [16, 64, 256] {
         let mut group = criterion.benchmark_group(format!("constraints {size}x{size}"));
-        group.sample_size(1000);
         let area = Rect::new(0, 0, size, size);
-        for constraint in ["Fill", "Length", "Max", "Min", "Percentage", "Ratio"] {
-            group.bench_function(constraint.to_string(), |bencher| {
-                bencher.iter(|| {
-                    _ = black_box(generate_layout(constraint, area));
-                });
-            });
-        }
+        group.bench_function("Fill", |bencher| {
+            bencher.iter(|| layout_fill(black_box(area)));
+        });
+        group.bench_function("Length", |bencher| {
+            bencher.iter(|| layout_length(black_box(area)));
+        });
+        group.bench_function("Max", |bencher| {
+            bencher.iter(|| layout_max(black_box(area)));
+        });
+        group.bench_function("Min", |bencher| {
+            bencher.iter(|| layout_min(black_box(area)));
+        });
+        group.bench_function("Percentage", |bencher| {
+            bencher.iter(|| layout_percentage(black_box(area)));
+        });
+        group.bench_function("Ratio", |bencher| {
+            bencher.iter(|| layout_ratio(black_box(area)));
+        });
         group.finish();
     }
 }
 
-criterion_group!(benches, constraints_render);
+fn layout_fill(area: Rect) -> Rc<[Rect]> {
+    Layout::vertical([Fill(1); SPLIT_BY as usize]).split(area)
+}
+
+fn layout_length(area: Rect) -> Rc<[Rect]> {
+    Layout::vertical([Length(area.width / SPLIT_BY); SPLIT_BY as usize]).split(area)
+}
+
+fn layout_max(area: Rect) -> Rc<[Rect]> {
+    Layout::vertical([Max(area.width / SPLIT_BY); SPLIT_BY as usize]).split(area)
+}
+
+fn layout_min(area: Rect) -> Rc<[Rect]> {
+    Layout::vertical([Min(area.width / SPLIT_BY); SPLIT_BY as usize]).split(area)
+}
+
+fn layout_percentage(area: Rect) -> Rc<[Rect]> {
+    Layout::vertical([Percentage(100 / SPLIT_BY); SPLIT_BY as usize]).split(area)
+}
+
+fn layout_ratio(area: Rect) -> Rc<[Rect]> {
+    Layout::vertical([Ratio(1, SPLIT_BY.into()); SPLIT_BY as usize]).split(area)
+}
+
+criterion_group!(benches, layout_split);
