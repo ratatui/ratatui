91fa249cd49aa3ad29ff4ee70b8fc6fe8bdad382
Shingo OKAWA <shingo.okawa.g.h.c@gmail.com>
2025-09-28 16:38:45 +0900

    chore: remove obsolete doc_auto_cfg feature (#2103)
    The doc_auto_cfg feature was incorporated into the doc_cfg feature
    in https://github.com/rust-lang/rust/pull/138907
    
    Closes #2102
    
    Signed-off-by: Shingo OKAWA <shingo.okawa.g.h.c@gmail.com>

--- ratatui-widgets/src/lib.rs
+++ ratatui-widgets/src/lib.rs
@@ -1,7 +1,6 @@
 #![no_std]
 // show the feature flags in the generated documentation
 #![cfg_attr(docsrs, feature(doc_cfg))]
-#![cfg_attr(docsrs, feature(doc_auto_cfg))]
 #![doc(
     html_logo_url = "https://raw.githubusercontent.com/ratatui/ratatui/main/assets/logo.png",
     html_favicon_url = "https://raw.githubusercontent.com/ratatui/ratatui/main/assets/favicon.ico"
