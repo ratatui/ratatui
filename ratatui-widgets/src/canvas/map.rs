24d5fe4dfcd5e3ed59a4dbd21cd18ce38fc5e0ce
Colton Loftus <70598503+C-Loftus@users.noreply.github.com>
2026-01-04 18:13:13 -0500

    minor tweak to capitalization

--- ratatui-widgets/src/canvas/map.rs
+++ ratatui-widgets/src/canvas/map.rs
@@ -31,7 +31,7 @@ impl MapResolution {
     }
 }
 
-/// A world map using a Plate carrée (equirectangular) projection. 
+/// A world map using a plate carrée (equirectangular) projection. 
 ///
 /// A world map can be rendered with different [resolutions](MapResolution) and [colors](Color).
 #[derive(Debug, Default, Clone, Eq, PartialEq, Hash)]
