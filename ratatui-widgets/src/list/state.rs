0e863cd68c68b3095164e9263fad3fd0468ab49b
Logan Ruf <Laython1316@gmail.com>
2026-03-07 20:38:43 -0800

    update(list): clamp selected in select() and add public item count API
    - Move clamping logic into select() so all selection methods automatically
      clamp when item_count is known.
    
    - Add set_item_count() and with_item_count() to let users set the
      item count before the first render.
    
    - Use set_item_count() in rendering to replace the manual bounds check.
    
    - Update documentation to reflect the new clamping behavior and add
      assertions to doc tests.
    
    - Gammar and other doc changes

--- ratatui-widgets/src/list/state.rs
+++ ratatui-widgets/src/list/state.rs
@@ -5,12 +5,14 @@
 /// that the selected item is visible. This will modify the [`ListState`] object passed to the
 /// `Frame::render_stateful_widget` method.
 ///
-/// The state consists of two fields:
+/// The state consists of three fields:
 /// - [`offset`]: the index of the first item to be displayed
 /// - [`selected`]: the index of the selected item, which can be `None` if no item is selected
+/// - [`item_count`]: the number of items in the list, set during rendering or manually
 ///
 /// [`offset`]: ListState::offset()
 /// [`selected`]: ListState::selected()
+/// [`item_count`]: ListState::item_count()
 ///
 /// See the list in the [Examples] directory for a more in depth example of the various
 /// configuration options and for how to handle state.
@@ -33,7 +35,7 @@
 /// let mut state = ListState::default();
 ///
 /// *state.offset_mut() = 1; // display the second item and onwards
-/// state.select(Some(3)); // select the forth item (0-indexed)
+/// state.select(Some(3)); // select the fourth item (0-indexed)
 ///
 /// frame.render_stateful_widget(list, area, &mut state);
 /// # }
@@ -67,7 +69,7 @@ impl core::hash::Hash for ListState {
 impl ListState {
     /// Sets the index of the first item to be displayed
     ///
-    /// This is a fluent setter method which must be chained or used as it consumes self
+    /// Fluent setter method which must be chained or used as it consumes self
     ///
     /// # Examples
     ///
@@ -84,7 +86,7 @@ impl ListState {
 
     /// Sets the index of the selected item
     ///
-    /// This is a fluent setter method which must be chained or used as it consumes self
+    /// Fluent setter method which must be chained or used as it consumes self
     ///
     /// # Examples
     ///
@@ -96,6 +98,23 @@ impl ListState {
     #[must_use = "method moves the value of self and returns the modified value"]
     pub const fn with_selected(mut self, selected: Option<usize>) -> Self {
         self.selected = selected;
+        self
+    }
+
+    /// Sets the number of items in the list
+    ///
+    /// Fluent setter method which must be chained or used as it consumes self
+    ///
+    /// # Examples
+    ///
+    /// ```rust
+    /// use ratatui::widgets::ListState;
+    ///
+    /// let state = ListState::default().with_item_count(Some(5));
+    /// ```
+    #[must_use = "method moves the value of self and returns the modified value"]
+    pub const fn with_item_count(mut self, count: Option<usize>) -> Self {
+        self.item_count = count;
         self
     }
 
@@ -147,6 +166,9 @@ impl ListState {
     ///
     /// Returns `None` if no item is selected
     ///
+    /// Note: this bypasses clamping. The `selected` value will be clamped on the next render
+    /// or navigation method call.
+    ///
     /// # Examples
     ///
     /// ```rust
@@ -159,16 +181,21 @@ impl ListState {
         &mut self.selected
     }
 
-    /// Returns the number of items in the list, if known.
+    /// Returns the number of items in the list, if known
     ///
-    /// This value is set during rendering. Returns `None` if the list hasn't been rendered yet.
+    /// This value is set during rendering or by calling [`set_item_count`](Self::set_item_count).
+    /// Returns `None` if the list hasn't been rendered yet.
     pub const fn item_count(&self) -> Option<usize> {
         self.item_count
     }
 
-    /// Sets the index of the selected item
+    /// Sets the number of items in the list
     ///
-    /// Set to `None` if no item is selected. This will also reset the offset to `0`.
+    /// This value is updated during rendering.
+    /// You can update it manually to enable clamping before the first render,
+    /// or change the upper bound of the clamp between renders.
+    ///
+    /// Note: This will immediately clamp the `selected` to be less than `item_count`.
     ///
     /// # Examples
     ///
@@ -176,13 +203,46 @@ impl ListState {
     /// use ratatui::widgets::ListState;
     ///
     /// let mut state = ListState::default();
+    /// state.set_item_count(Some(5));
+    /// ```
+    pub const fn set_item_count(&mut self, count: Option<usize>) {
+        self.item_count = count;
+        self.clamp_selected();
+    }
+
+    /// Sets the index of the selected item
+    ///
+    /// Set to `None` if no item is selected. This will also reset the offset to `0`.
+    ///
+    /// If the `item_count` is known (either from rendering or from calling
+    /// [`set_item_count`](Self::set_item_count)), the `selected` index is clamped to valid bounds:
+    /// indices past the last item are clamped to the last item.
+    ///
+    /// If the `item_count` is zero, any `selected` index results in `None`.
+    ///
+    /// # Examples
+    ///
+    /// ```rust
+    /// use ratatui::widgets::ListState;
+    ///
+    /// let mut state = ListState::default();
+    /// // Before `item_count` is set
     /// state.select(Some(1));
+    /// assert_eq!(state.selected(), Some(1));
+    ///
+    /// state.set_item_count(Some(5));
+    /// state.select(Some(5)); // out of bounds (0-indexed)
+    /// assert_eq!(state.selected(), Some(4));
+    ///
+    /// state.set_item_count(Some(0)); // `selected` updated
+    /// assert_eq!(state.selected(), None);
     /// ```
     pub const fn select(&mut self, index: Option<usize>) {
         self.selected = index;
         if index.is_none() {
             self.offset = 0;
         }
+        self.clamp_selected();
     }
 
     /// Clamps the `selected` index to valid bounds if `item_count` is known.
@@ -200,8 +260,9 @@ impl ListState {
 
     /// Selects the next item or the first one if no item is selected
     ///
-    /// Note: until the list is rendered, the number of items is not known, so the index is set to
-    /// `0` and will be corrected when the list is rendered
+    /// Note: until the list is rendered, the number of items is not known, so the `selected` index
+    /// is set to `0`. After the item count is known, the `selected` index will be clamped to the
+    /// last item in the list.
     ///
     /// # Examples
     ///
@@ -210,17 +271,18 @@ impl ListState {
     ///
     /// let mut state = ListState::default();
     /// state.select_next();
+    /// assert_eq!(state.selected(), Some(0));
     /// ```
     pub fn select_next(&mut self) {
         let next = self.selected.map_or(0, |i| i.saturating_add(1));
         self.select(Some(next));
-        self.clamp_selected();
     }
 
     /// Selects the previous item or the last one if no item is selected
     ///
-    /// Note: until the list is rendered, the number of items is not known, so the index is set to
-    /// `usize::MAX` and will be corrected when the list is rendered
+    /// Note: until the list is rendered, the number of items is not known, so the `selected` index
+    /// is set to `usize::MAX`. After the item count is known, the `selected` index will be clamped
+    /// to the last item in the list.
     ///
     /// # Examples
     ///
@@ -229,6 +291,7 @@ impl ListState {
     ///
     /// let mut state = ListState::default();
     /// state.select_previous();
+    /// assert_eq!(state.selected(), Some(usize::MAX));
     /// ```
     pub fn select_previous(&mut self) {
         let current = self.item_count.map_or(usize::MAX, |c| c.saturating_sub(1));
@@ -238,8 +301,8 @@ impl ListState {
 
     /// Selects the first item
     ///
-    /// Note: until the list is rendered, the number of items is not known, so the index is set to
-    /// `0` and will be corrected when the list is rendered
+    /// Sets the `selected` index to `0`, unless the `item_count` is zero, then `selected` is set
+    /// to `None`.
     ///
     /// # Examples
     ///
@@ -248,6 +311,7 @@ impl ListState {
     ///
     /// let mut state = ListState::default();
     /// state.select_first();
+    /// assert_eq!(state.selected(), Some(0));
     /// ```
     pub const fn select_first(&mut self) {
         self.select(Some(0));
@@ -255,8 +319,9 @@ impl ListState {
 
     /// Selects the last item
     ///
-    /// Note: until the list is rendered, the number of items is not known, so the index is set to
-    /// `usize::MAX` and will be corrected when the list is rendered
+    /// Note: until the list is rendered, the number of items is not known, so the `selected` index
+    /// is set to `usize::MAX`. After the item count is known, the `selected` index will be clamped
+    /// to the last item in the list.
     ///
     /// # Examples
     ///
@@ -265,17 +330,18 @@ impl ListState {
     ///
     /// let mut state = ListState::default();
     /// state.select_last();
+    /// assert_eq!(state.selected(), Some(usize::MAX));
     /// ```
     pub fn select_last(&mut self) {
         let last = self.item_count.map_or(usize::MAX, |c| c.saturating_sub(1));
         self.select(Some(last));
     }
 
-    /// Scrolls down by a specified `amount` in the list.
+    /// Scrolls down by a specified `amount` in the list
     ///
-    /// This method updates the selected index by moving it down by the given `amount`.
-    /// If the `amount` causes the index to go out of bounds (i.e., if the index is greater than
-    /// the length of the list), the last item in the list will be selected.
+    /// This method updates `selected` by moving it down by the given `amount`.
+    /// If the `item_count` is known and the `amount` causes `selected` to exceed it,
+    /// the last item in the list will be selected.
     ///
     /// # Examples
     ///
@@ -283,19 +349,20 @@ impl ListState {
     /// use ratatui::widgets::ListState;
     ///
     /// let mut state = ListState::default();
+    /// state.select(Some(2));
     /// state.scroll_down_by(4);
+    /// assert_eq!(state.selected(), Some(6));
     /// ```
     pub fn scroll_down_by(&mut self, amount: u16) {
         let selected = self.selected.unwrap_or_default();
         self.select(Some(selected.saturating_add(amount as usize)));
-        self.clamp_selected();
     }
 
-    /// Scrolls up by a specified `amount` in the list.
+    /// Scrolls up by a specified `amount` in the list
     ///
-    /// This method updates the selected index by moving it up by the given `amount`.
-    /// If the `amount` causes the index to go out of bounds (i.e., less than zero),
-    /// the first item in the list will be selected.
+    /// This method updates `selected` by moving it up by the given `amount`.
+    /// If the `amount` causes `selected` to go below zero, the first item in the list
+    /// will be selected.
     ///
     /// # Examples
     ///
@@ -303,7 +370,9 @@ impl ListState {
     /// use ratatui::widgets::ListState;
     ///
     /// let mut state = ListState::default();
+    /// state.select(Some(2));
     /// state.scroll_up_by(4);
+    /// assert_eq!(state.selected(), Some(0));
     /// ```
     pub fn scroll_up_by(&mut self, amount: u16) {
         let selected = self.selected.unwrap_or_default();
